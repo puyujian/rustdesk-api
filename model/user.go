62f829d2dfbe671b2531e43112db8b7ade5b1e77
luo-bo <603509782@qq.com>
2025-12-29 14:35:29 +0800

    ``` feat(user): 添加用户订阅和订单路由权限
    - 在UserRouteNames中新增"MySubscription"和"MyOrders"路由权限
    - 使用户能够访问订阅和订单相关功能
    ```

--- model/user.go
+++ model/user.go
@@ -30,6 +30,6 @@ type UserList struct {
 }
 
 var UserRouteNames = []string{
-	"MyTagList", "MyAddressBookList", "MyInfo", "MyAddressBookCollection", "MyPeer", "MyShareRecordList", "MyLoginLog",
+	"MyTagList", "MyAddressBookList", "MyInfo", "MyAddressBookCollection", "MyPeer", "MyShareRecordList", "MyLoginLog", "MySubscription", "MyOrders",
 }
 var AdminRouteNames = []string{"*"}
